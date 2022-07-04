47e9e9aec3a3e2fd6c61c10ac39ffa1d8eb03256
Eric Curley <ErkCurley@users.noreply.github.com>
2022-07-03 22:08:42 -0400

    Also fill containers with energy

--- role.harvester.js
+++ role.harvester.js
@@ -10,15 +10,15 @@ var roleHarvester = {
         var sources = creep.room.find(FIND_SOURCES);
         
         if(creep.store.getFreeCapacity() > 0) {
-            console.log(creep.memory.sourceTarget)
             if(creep.harvest(sources[creep.memory.sourceTarget]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[creep.memory.sourceTarget], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }else{
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
+                    return (structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_CONTAINER ||
                         structure.structureType == STRUCTURE_TOWER) &&
                         structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
