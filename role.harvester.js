42e726f14119d11dbdec7a32219b963451d982fc
Eric Curley <ErkCurley@users.noreply.github.com>
2022-07-08 02:04:55 -0400

    targets > containers

--- role.harvester.js
+++ role.harvester.js
@@ -2,6 +2,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+var spawnName = "Home"
 
 var roleHarvester = {
     /** @param {Creep} creep **/
@@ -52,18 +53,23 @@ var roleHarvester = {
             });
             
             if(targets.length == 0) {
-                var targets = creep.room.find(FIND_STRUCTURES, {
+                var containers = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_CONTAINER) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
                 });
-            }
-            
-            if(targets.length > 0) {
+                
+                if(containers.length > 0){
+                    if(creep.transfer(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(containers[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }else{
                     creep.moveTo(Game.spawns[spawnName]);
                 }
+                
+                
+            }else{
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
